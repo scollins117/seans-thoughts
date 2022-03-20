32687806f4cbb3965fdb49f20883cbfcaebdfa47
unknown <seancollins117@gmail.com>
2022-03-20 15:20:02 -0700

    updated create user method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -41,21 +41,8 @@ getAllUser(req, res) {
   // createUser
   createUser({ body }, res) {
     User.create(body)
-      .then(({ _id }) => {
-          return User.findOneAndUpdate(
-              {_id: body.userId},
-              { $push: {Users: _id }},
-              { new: true }
-            );
-        })
-        .then(dbUserData => {
-          if (!dbUserData) {
-          res.status(404).json({ message: 'No User found with this id!' });
-          return;
-          }
-          res.json(dbUserData);
-        })
-        .catch(err => res.json(err));
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
   },
 
   // update User by id
