365ee0eb656ac379166a26f52db0a36ac54b4968
unknown <seancollins117@gmail.com>
2022-03-20 15:37:07 -0700

    add and remove friend functonality added

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -69,6 +69,34 @@ getAllUser(req, res) {
         res.json(dbUserData);
       })
       .catch(err => res.json(err));
+  },
+
+  //add friend
+  addFriend({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.UserId },
+      { $push: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  //remove friend
+  removeReply({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendID } },
+      { new: true }
+    )
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
   }
 };
 
